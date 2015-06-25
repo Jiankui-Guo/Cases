e6c1bdc19c3c6e0331fd901bc8b63503e5b95ec5
Jiankui-Guo <jiankuiguo@163.com>
2015-06-26 07:52:47 +0800

    Add event receiver for doc deletion and update

---
+++ DocumentReadStatus/DocReadStatusHandler/DocDeleteHandler.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Security.Permissions;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Utilities;
+using Microsoft.SharePoint.Workflow;
+
+namespace DocumentReadStatus.DocReadStatusHandler
+{
+    /// <summary>
+    /// List Item Events
+    /// </summary>
+    public class DocReadStatusHandler : SPItemEventReceiver
+    {
+        /// <summary>
+        /// An item was updated.
+        /// </summary>
+        public override void ItemUpdated(SPItemEventProperties properties)
+        {
+            base.ItemUpdated(properties);
+
+            CleanUpDocReadStatus(properties.SiteId,
+                properties.Web.ID,
+                properties.ListItem.Url);
+        }
+
+        /// <summary>
+        /// An item was deleted.
+        /// </summary>
+        public override void ItemDeleted(SPItemEventProperties properties)
+        {
+            base.ItemDeleted(properties);
+
+            CleanUpDocReadStatus(properties.SiteId,
+                properties.Web.ID,
+                properties.ListItem.Url);
+        }
+
+        private void CleanUpDocReadStatus(Guid siteId, Guid webId, string itemUrl)
+        {
+            SPSecurity.RunWithElevatedPrivileges(() =>
+            {
+                using (SPSite site = new SPSite(siteId))
+                {
+                    using (SPWeb web = site.OpenWeb(webId))
+                    {
+                        SPList readStatusList = web.Lists["DocReadStatus"];
+
+                        if (readStatusList == null)
+                        {
+                            return;
+                        }
+
+                        SPQuery query = new SPQuery();
+                        query.Query = string.Format(string.Concat(
+                          "<Where><Eq>",
+                             "<FieldRef Name='Title'/>",
+                             "<Value Type='Text'>{0}</Value></Eq>",
+                          "</Where>"), "/" + itemUrl);
+
+                        query.ViewFields = "<FieldRef Name='Title' /><FieldRef Name='ViewPeople' />";
+                        query.ViewFieldsOnly = true;
+
+                        SPListItemCollection items = readStatusList.GetItems(query);
+
+                        if (items.Count > 0)
+                        {
+                            site.AllowUnsafeUpdates = true;
+                            web.AllowUnsafeUpdates = true;
+
+                            foreach (SPListItem item in items)
+                            {
+                                readStatusList.Items.DeleteItemById(item.ID);
+                            }
+
+                            web.AllowUnsafeUpdates = false;
+                            site.AllowUnsafeUpdates = false;
+                        }
+
+                    }
+                }
+            });
+        }
+    }
+}
